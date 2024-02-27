a0edcf7919bdc7d3cdb57fb49348acf3a31a0767
Edwin Roman <romanseberino@gmail.com>
2024-02-27 14:16:28 -0400

    Basic ATM recursion algorithm

--- 3rd-week-functions-and-clases/recursive-functions.py
+++ 3rd-week-functions-and-clases/recursive-functions.py
@@ -179,3 +179,59 @@ def binary_s(array, target):
     
 
 print("Found element:", binary_s(sorted([1,2,3,4,32,3,5,7,6]), 4))
+
+def atm_with_recursion():
+    
+    papeletas = [100,200,500,1000]
+    option_bank = 0
+
+    def Proceso(monto):
+        for papeleta in papeletas:
+            #print(monto//b)
+            if(monto/papeleta == 1):
+                print('Se le ha dado 1 billete de {}'.format(papeleta))
+                exit()
+
+            if((monto//papeleta) != 0):
+                devueltas.append([monto//papeleta,papeleta])
+        
+        devueltas_finales = sorted(devueltas)
+        #Con esto se puede lograr pagar con las papeletas mayores, lo cual  puedo dar las de mayor valor que seria menos
+        #fisicamente que es lo que se logra o persive buscar. (LOGICO MUCHO, FISICO POCO.)
+
+        #print(devueltas_finales)
+
+        for dev in devueltas_finales:
+            print('Se le ha dado ',dev[0],' billetes de ',dev[1])
+            monto = monto - (dev[0]*dev[1])
+            #break (Para comprobar el monto o la valides de la operacion)
+            if(monto >= 100):
+                Proceso(monto)
+            else:
+                exit()
+
+        print('MOnto total: {}'.format(monto))
+        #FIN FUNCION
+
+    print('Cajero  Automatico.net')
+
+    monto_cuenta = int(input('Ingrese su monto a retirar: '))
+
+    print('(1) FDP Inversments(Bank) \n(2) Any Bank')
+    option_bank = int(input())
+
+
+    if(monto_cuenta == 0 or monto_cuenta < 100):
+        print('Sea conciente, debe retirar 100 pesos en adelante.')
+        exit()
+
+    if(option_bank == 1):
+        if(monto_cuenta > 20000):
+            print('Lo sentimos usted no puede solicitar mas de 20000 pesos.')
+        else:
+            devueltas = []
+        Proceso(monto_cuenta)
+    else:
+        if(monto_cuenta > 10000):
+            print('Lo sentimos usted no puede solicitar mas de 10000 pesos.')
+            print('El otro banco para la próxima, esta en desarrollo. (COMING SOON)')
